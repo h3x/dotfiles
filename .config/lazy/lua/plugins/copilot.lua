6e8a07d9809dfdc8f4d9ace5761b9b32a3b655c3
Adam Austin <adamjaustin84@gmail.com>
2024-03-19 19:03:51 +1100

    Update

--- .config/lazy/lua/plugins/copilot.lua
+++ .config/lazy/lua/plugins/copilot.lua
@@ -1,24 +1,22 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<C-y>",
-            accept_word = false,
-            accept_line = false,
-            next = "<C-n>",
-            prev = "<C-p>",
-            dismiss = "<ESC>",
-          },
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-y>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-n>",
+          prev = "<C-p>",
+          dismiss = "<ESC>",
         },
-      })
-    end,
-  },
+      },
+    })
+  end,
 }
