c380ab0c79acb86518065c72dcef050227ecda99
Adam Austin <adam.austin@alayacare.com>
2024-04-13 12:48:18 +1000

    Add some error checking in connect script to validate json before proceesing

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -4,8 +4,13 @@
 #         echo "Usage: connect.sh <tenant> <environment>"
 #         exit 1
 # fi
-#
-#
+
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+RED="\033[0;31m"
+MAGENTA="\033[0;35m"
+NC="\033[0m"
+
 NABU="$ALAYACARE_HOME/services/nabu/web/api/v1/tenant"
 LOCAL_FILE="alayadev.json"
 TEMP_FILE="alayadev.tmp.json"
@@ -15,16 +20,25 @@ TEMP="${NABU}/${TEMP_FILE}"
 
 input=$(cat)
 
-[[ -z $input ]] && echo "No input from nabu" && exit 1
+[[ -z $input ]] && echo "${RED}No input from nabu${NC}" && exit 1
 
+set +e
+errors=$(jq -re '""' <<<"${input}" 2>&1)
+if [ ! -z "${errors}" ]; then
+  echo -e "${RED}Error parsing input: ${errors}${NC}"
+  exit 1
+fi
 
 if [ -f $TEMP ]; then
-        echo "Temporary file already exists. Please reset before connecting"
+        echo -e "${MAGENTA}Temporary file already exists. Please reset before connecting${NC}"
         exit 1
 fi
 
+set -e
+
 mv $LOCAL_TENANT $TEMP
-echo "Temporary file ${TEMP_FILE} created successfully!"
+echo -e "${BLUE}Temporary file ${TEMP_FILE} created successfully!${NC}"
 
 jq --argjson mysql "$(echo $input | jq '.databases.mysql')" '.databases.mysql=$mysql' $TEMP > $LOCAL_TENANT
 
+echo -e "${GREEN}Connected to remote tenant successfully!${NC}"
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+RED="\033[0;31m"
+MAGENTA="\033[0;35m"
+NC="\033[0m"
+
 NABU="$ALAYACARE_HOME/services/nabu/web/api/v1/tenant"
 LOCAL_FILE="alayadev.json"
 TEMP_FILE="alayadev.tmp.json"
@@ -9,10 +15,10 @@ TEMP="${NABU}/${TEMP_FILE}"
 
 
 if [ ! -f $TEMP ]; then
-        echo "Temporary file does not exist. Exiting..."
+        echo -e "${RED}Temporary file does not exist. Exiting...${NC}"
         exit 1
 fi
 mv $TEMP $LOCAL_TENANT 
 
-echo "${LOCAL_FILE} Reset Successful!"
+echo -e "${GREEN}${LOCAL_FILE} Reset Successful!${NC}"
 echo ""
