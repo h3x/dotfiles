c5ea0d50f0acf2d312d5bcb3dc27958e851512eb
Adam Austin <adam.austin@alayacare.com>
2025-04-01 13:39:42 +1100

    Update

--- .config/nvim11/lua/peekabooty/plugins/telescope.lua
+++ .config/nvim11/lua/peekabooty/plugins/telescope.lua
@@ -32,72 +32,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
     pcall(require('telescope').load_extension, 'harpoon')
     pcall(require('telescope').load_extension, 'rest')
 
-    -- -- Define a custom function that calls find_files with custom filtering
-    -- local function custom_find_files()
-    --   local should_include_tests = false
-    --   local filter_out_test_files = function(results)
-    --     local filtered_results = {}
-    --     for _, result in ipairs(results) do
-    --       if not string.match(result.display, 'tests/') then
-    --         table.insert(filtered_results, result)
-    --       end
-    --     end
-    --     return filtered_results
-    --   end
-    --
-    --   require('telescope.builtin').find_files {
-    --     attach_mappings = function(_, map)
-    --       map('i', '<cr>', function(prompt_bufnr)
-    --         local selection = require('telescope.actions.state').get_selected_entry()
-    --         require('telescope.actions').close(prompt_bufnr)
-    --         vim.cmd(string.format('e %s', selection.path))
-    --       end)
-    --       return true
-    --     end,
-    --     filter = should_include_tests and nil or filter_out_test_files,
-    --   }
-    -- end
-    --
-    -- -- Define the keymap with the custom function
-    -- vim.keymap.set('n', '<leader>ff', custom_find_files, { desc = '[S]earch [F]iles' })
-
-    -- local no_test_picker = function(opts)
-    --   local find_files = require('telescope.finders').new {
-    --     fn_command = function(opts)
-    --       return {
-    --         command = 'find',
-    --         args = { opts.cwd, '-type', 'f', '-name', '*' .. opts.file_ext .. '*', '-not', '-path', '*test_*' },
-    --       }
-    --     end,
-    --     entry_maker = require('telescope.finders.entry').get_from_file(opts),
-    --   }
-    --   local file_browser = require('telescope.sorters').get_generic_fuzzy_sorter()
-    --   local make_entry = require 'telescope.make_entry'
-    --   local path_display = require('telescope.actions.state').path_display
-    --   local conf = require('telescope.config').values
-    --   return require('telescope.pickers').new(opts, {
-    --     finder = find_files,
-    --     sorter = file_browser,
-    --     previewer = conf.file_previewer(opts),
-    --     prompt_title = 'Find Files (without tests)',
-    --     results_title = 'Files (without tests)',
-    --     cwd = opts.cwd,
-    --   })
-    -- end
-    --
-    -- local no_test_files = function(opts)
-    --   opts = opts or {}
-    --   opts.file_ext = opts.file_ext or ''
-    --   require('telescope.builtin').find_files(no_test_picker(opts))
-    -- end
-    --
-    -- vim.cmd [[command! -nargs=? NoTestFiles lua no_test_files({prompt_title="Files (no test files)", file_ext="*"})]]
-    --
-    -- vim.keymap.set('n', '<leader>ff', no_test_files, { desc = '[S]earch [F]iles (no tests)' })
-
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>ff', ':lua require("telescope.builtin").find_files({ hidden=true })<CR>', { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>ft', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -107,16 +44,10 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>fa', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    -- TODO: Add leader a for document symbol search
+    -- vim.keymap.set("n", "<leader>a", builtin.lsp_document_symbols({symbols={"method","function"}}) , { desc = "[S]earch [A]ll functions" })
 
     vim.keymap.set('v', '<leader>fg', 'zy:Telescope live_grep default_text="<C-r>z<CR>"', { desc = '[S]earch by [G]rep' })
-    -- default_text = table.concat(get_selection())
-    --
-    -- TODO: Adam make this work. maybe upgrade to nvim nightly
-    -- local get_selection = function()
-    --   return vim.fn.getregion(vim.fn.getpos '.', vim.fn.getpos 'v', { mode = vim.fn.mode() })
-    -- end
-    --
-    -- vim.keymap.set('v', '<leader>fg', builtin.live_grep { default_text = table.concat(get_selection()) }, { desc = '[S]earch by [G]rep' })
 
     vim.keymap.set('n', '<leader>S', function()
       builtin.grep_string {
